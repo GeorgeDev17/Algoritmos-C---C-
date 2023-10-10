26e615d433fa72f6caf4eb6c41b03374c6bba496
Jorge Alberto Bedoya Montoya <jorge.bedoya1@utp.edu.co>
2023-10-10 04:18:37 +0200

    Add files via upload

---
+++ Algoritmos/10.Octubre/09-10-23/1.Funciones.cpp
@@ -0,0 +1,23 @@
+/*Autor: Jorge Alberto Bedoya Montoya
+Dev C++ versión: 5.11
+Plataforma OS: Parrot Linux 6.1.0
+Ultima actualización: 09-10-2023*/
+
+/*--------------------------------------------------------------------------------------------------
+Algoritmos Lenguaje C
+    --- Funciones y procedimientos---
+ Calcula la suma de dos números y luego imprime el resultado
+--------------------------------------------------------------------------------------------------*/
+# include <iostream>
+
+using namespace std;
+
+int suma (int a, int b){ // Función suma que toma 2 parámetros enteros (a y b).
+	int sum;
+	sum = a + b; 
+	return sum;	
+}
+	int main(){
+		cout << suma (5, 6); //Impresión por consola del resultado.
+		return 0;
+}
